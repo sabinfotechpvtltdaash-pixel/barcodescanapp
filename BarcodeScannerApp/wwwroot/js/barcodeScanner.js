54d0873066fb77228a4ef0f7284549e340031dc8
sabinfotechpvtltdaash-pixel <sabinfotechpvtltdaash@gmail.com>
2026-08-14 16:36:28 +0530

    first commit

---
+++ BarcodeScannerApp/wwwroot/js/barcodeScanner.js
@@ -0,0 +1,37 @@
+﻿let controls = null;
+let lastValue = "";
+
+window.barcodeScanner = {
+    start: async function (dotNetHelper) {
+        const video = document.getElementById("barcodeVideo");
+        const reader = new ZXingBrowser.BrowserMultiFormatReader();
+
+        controls = await reader.decodeFromConstraints(
+            {
+                audio: false,
+                video: {
+                    facingMode: { ideal: "environment" }
+                }
+            },
+            video,
+            (result) => {
+                if (result) {
+                    const value = result.getText();
+
+                    if (value !== lastValue) {
+                        lastValue = value;
+                        dotNetHelper.invokeMethodAsync("BarcodeDetected", value);
+                    }
+                }
+            });
+    },
+
+    stop: function () {
+        if (controls) {
+            controls.stop();
+            controls = null;
+        }
+
+        lastValue = "";
+    }
+};
